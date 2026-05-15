bf47a98f8ca48f1b87e3ca29b826b950033270b6
Adrian Shum <adrian@cshum.com>
2026-05-16 01:51:58 +0800

    fix: librsvg fix

--- install-rust.sh
+++ install-rust.sh
@@ -2,5 +2,9 @@
 
 set -euo pipefail
 
+PATH="/root/.cargo/bin:$PATH"
+
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs \
   | sh -s -- -y --profile minimal --default-toolchain stable
+
+cargo install cargo-c
